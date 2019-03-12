1a7c5d624070e18ff07c71fb989cee0903136bd7
Bry Cox <carl.cc@gmail.com>
2019-03-12 13:56:22 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,9 @@ class Triangle
   end
 
   def kind
-    if self.s1 == self.s2 && self.s2 == self.s3 && self.s3 == self.s1
+    if self.s1 <= 0 || self.s2 <= 0
+      raise TriangleError
+    elsif self.s1 == self.s2 && self.s2 == self.s3 && self.s3 == self.s1
       :equilateral
     elsif self.s1 == self.s2 || self.s2 == self.s3 || self.s3 == self.s1
       :isosceles
