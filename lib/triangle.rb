ff16b7953c70c2c3b3146642c050c8a178678476
Bry Cox <carl.cc@gmail.com>
2019-03-12 12:41:43 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,17 @@
+require 'pry'
 class Triangle
-  # write code here
+
+  attr_reader :s1, :s2, :s3
+
+  def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end
+
+  def kind
+  if self.s1 == self.s2 && self.s2 == self.s3 && self.s1 > 0 && self.s2 > 0 && self.s3 > 0
+    :equilateral
+  end
+end
 end
